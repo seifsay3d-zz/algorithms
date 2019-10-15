358036fc9ac48ff958ebcf1b25f5e99e9b8edc3f
Seif Sayed <seifsayed@gmail.com>
2019-10-15 17:26:50 +0200

    optimize bubble sort

--- bubbleSort.ts
+++ bubbleSort.ts
@@ -1,14 +1,19 @@
-let arrayToSort = [5, 1, 4, 2, 8];
+let arrayToSort = [1, 2, 4, 5, 8];
 
 function bubbleSort(array) {
   for (let i = 0; i < array.length; i++) {
+    let hasBeenSwapped = false;
     for (let j = 0; j < array.length; j++) {
       if (array[i] > array[j]) {
         let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
+        hasBeenSwapped = true;
       }
     }
+    if (!hasBeenSwapped) break;
   }
   return array;
 }
+
+console.log(bubbleSort(arrayToSort));
