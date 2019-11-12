e67190a5a76b200b17110f66dd9ec25b4c56fcd1
peterkaram <Petertalaat@hotmail.com>
2019-11-12 15:19:53 +0200

    Cleaning code

--- bubbleSort.ts
+++ bubbleSort.ts
@@ -2,16 +2,17 @@ let arrayToSort = [1, 2, 4, 5, 8];
 
 function bubbleSort(array) {
   for (let i = 0; i < array.length; i++) {
-    let hasBeenSwapped = false;
-    for (let j = 0; j < array.length; j++) {
-      if (array[i] > array[j]) {
+    let isSorted = true;
+    for (let j = 0; j < array.length; j++) {      
+      if (array[i] < array[j]) {
+        continue;
+      }
         let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
-        hasBeenSwapped = true;
-      }
+        isSorted = false;
     }
-    if (!hasBeenSwapped) break;
+    if (isSorted) return array;
   }
   return array;
 }
